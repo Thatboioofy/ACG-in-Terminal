c884a89f2f7888d6d7fee3a62e4b57fc5ee2ec6a
thatboioofy <maksadlerk@gmail.com>
2026-01-09 18:38:32 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -2,16 +2,45 @@ import keyboard
 import time
 import os
 import random
+from pick import pick
 from termcolor import cprint
 menu = "main"
+Sys_Int = True
+
+#Code still in really early Development and everything IS broken
 
 
-def MainScreen(menu):
+def CoolentPumps(PumpNum, Pump_Strain, Pump_Strength):
+    print()
+
+
+
+
+
+
+
+def MainScreen(menu, Sys_Int):
     loop = True
     CoreTemp = 0
     print("PRC Managment System v0.1")
     print("Solaris Copyright 1999")
     print("\n")
+    if Sys_Int == True:
+        Sys_Int = False
+        default_Pump_Strength = 5 # Sets initial pump speeds (NOTE : 7.5 will start cause the pumps to gain strain)
+        enable_pump_strain = True
+
+        Pump1_Strain = 0
+        Pump1_Strength = default_Pump_Strength
+        
+        Pump2_Strain = 0
+        Pump2_Strength = default_Pump_Strength
+        
+        Pump3_Strain = 0
+        Pump3_Strength = default_Pump_Strength
+        
+        Pump4_Strain = 0
+        Pump4_Strength = default_Pump_Strength
     while loop == True:
         CoreTemp = CoreTemp + random.randint(20,100)
         if menu == "main":
@@ -25,26 +54,13 @@ def MainScreen(menu):
                 print("Reactor Tempreture :" + str(CoreTemp), end='\r')
             if keyboard.is_pressed("c") == True:
                 menu = "Coolent"
+            if keyboard.is_pressed("q") == True:
+                Startup('UNSHUT', 'main')
         if menu == "Coolent":
-            print("Coolent Configuration")
-            Position = 1
-            if keyboard.is_pressed("m") == True:
-                menu = "main"
-            if keyboard.is_pressed("down") == True:
-                if Position >= 5:
-                    Position = 5
-                else:
-                    Position = Position +1
-            if keyboard.is_pressed("up") == True:
-                if Position <= 1:
-                    Position = 1
-                else:
-                    Position = Position -1
-            
-            print("Coolent Pump 1", end='\r')
-            print("Coolent Pump 2", end='\r')
-            print("Coolent Pump 3", end='\r')
-            print("Coolent Pump 4", end='\r')
+            option = pick(['Coolent Pump 1', 'Coolent Pump 2', 'Coolent Pump 3', 'Coolent Pump 4'], 'Coolent Configuration')
+
+
+
         
 
             
@@ -75,7 +91,7 @@ def Startup(error, menu):
         time.sleep(random.uniform(0.1, 0.5))
 
     os.system('cls')
-    MainScreen(menu)
+    MainScreen(menu, Sys_Int)
 Startup("", menu)
 
 
